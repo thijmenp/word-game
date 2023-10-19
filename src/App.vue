d7ed24cfea784c8e3b4bf4d758551c90b45e8e96
Thijmen Petersen <t.petersen@arcady.nl>
2023-10-19 21:17:48 +0200

    boilerplate and banner

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Header from './components/Header.vue'
+</script>
+
+<template>
+  <div class="wrapper">
+      <Header />
+  </div>
+  <main>
+
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+    <header>
+      <h1>Word Game</h1>
+    </header>
+</template>
+<script setup>  
+</script>
